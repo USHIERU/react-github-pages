4a8328c8339a24641393b9cfa1d86101ba7184c7
Ushieru Kokoran <uzielcocolan@gmail.com>
2020-08-14 11:33:08 -0500

    Updates

---
+++ precache-manifest.f2605bc59186ca22c75e8019f5b41016.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f02bbf75454759aa24ab5046f6e4bea",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "9bdfbb504b26e7ccf063",
+    "url": "/react-github-pages/static/js/2.29c2d377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-pages/static/js/2.29c2d377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a4162767337f280d7b3",
+    "url": "/react-github-pages/static/js/main.7ddeee87.chunk.js"
+  },
+  {
+    "revision": "549506727088d589a102",
+    "url": "/react-github-pages/static/js/runtime-main.cdc1f5da.js"
+  }
+]);
