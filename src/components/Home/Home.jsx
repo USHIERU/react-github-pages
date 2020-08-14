5f5db96aa5936d741fc1bd390279233e6aa140c3
Ushieru Kokoran <uzielcocolan@gmail.com>
2020-08-14 11:28:15 -0500

    git test

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Navbar(props) {
+    return <>
+        <div className="container">
+            <div class="row">
+                <div class="col s12">
+                    <div class="card grey lighten-5">
+                        <div class="card-content">
+                            <span class="card-title">Card Title</span>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, voluptatum magnam! Dolor aliquam corporis, velit quaerat quasi quibusdam alias hic? Consequatur unde debitis voluptatibus soluta aspernatur, quae laudantium assumenda atque fuga a adipisci quidem harum vitae asperiores voluptatum non nam, omnis veniam ratione? Accusantium ratione voluptas iste provident quae dolorum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>;
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar(props) {
+    return <>
+        <nav>
+            <div class="nav-wrapper blue darken-4">
+                <a href="#" class="brand-logo center">React Github Pages</a>
+            </div>
+        </nav>
+    </>;
+}
